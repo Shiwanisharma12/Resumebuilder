7e0cffc334475abe2cac540f3380c7955b0ee305
Shiwani Sharma <shivanis@successive.com>
2020-07-13 14:20:38 +0530

    latest code

--- src/main/java/net/test/Pages/Resumepage.java
+++ src/main/java/net/test/Pages/Resumepage.java
@@ -1,18 +1,21 @@
 package net.test.Pages;
-import org.openqa.selenium.WebElement;
+
+import org.junit.Assert;
+import org.openqa.selenium.*;
 import com.typesafe.config.Config;
 import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.core.pages.WebElementFacade;
 import net.test.utilities.ConfigLoader;
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.By;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
-import java.security.cert.X509Certificate;
-import java.util.ArrayList;
+import org.openqa.selenium.support.ui.Select;
+
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class Resumepage extends PageObject {
+
+
 
 
     Config config = ConfigLoader.load();
@@ -21,57 +24,504 @@ public class Resumepage extends PageObject {
     WebElementFacade login;
 
     @FindBy(xpath = "//input[@type='email']")
-    WebElement EMAIL;
+    WebElementFacade EMAIL;
 
     @FindBy(xpath = "//span[text()='Next']")
-    WebElementFacade next;
+    WebElementFacade Next;
 
     @FindBy(xpath = "//input[@type='password']")
     WebElementFacade Password;
 
-    public String e_mail;
+    @FindBy(xpath = "//*[@id='content']/div/div/div[1]/div/a")
+    WebElementFacade createResumes;
+
+    @FindBy(xpath = "//input[@id='title']")
+    WebElementFacade Title;
+
+    @FindBy(xpath = "//input[@id='headline']")
+    WebElementFacade Headline;
+
+    @FindBy(xpath = "//input[@id='phone']")
+    WebElementFacade Phone;
+
+    @FindBy(xpath = "//input[@id='totalExperienceInMonths']")
+    WebElementFacade totalExperienceInMonths;
+
+    @FindBy(xpath = "//input[@id='linkedIn']")
+    WebElementFacade LinkedIn;
+
+    @FindBy(xpath = "//input[@id='githublink']")
+    WebElementFacade githublink;
+
+
+    @FindBy(xpath = "//input[@id='bitbucket']")
+    WebElementFacade Bitbucket;
+
+    @FindBy(xpath = "//input[@id='stacklink']")
+    WebElementFacade stacklink;
+
+    @FindBy(xpath = "//input[@id='hackerRank']")
+    WebElementFacade HackerRank;
+
+    @FindBy(xpath = "(//input[@placeholder='Technologies'])[1]")
+    WebElementFacade Technologies;
+
+    @FindBy(xpath = "(//input[@placeholder='Database'])[2]")
+    WebElementFacade Database;
+
+    @FindBy(xpath = "(//input[@placeholder='Frameworks'])[1]")
+    WebElementFacade Framework;
+
+
+    @FindBy(xpath = "(//input[@placeholder='OS'])[1]")
+    WebElementFacade OS;
+
+    @FindBy(xpath = "(//input[@placeholder='Third-Party Libraries '])[1]")
+    WebElementFacade thirdparty;
+
+    @FindBy(xpath = "(//input[@placeholder='Rewards & Recognition'])[1]")
+    WebElementFacade rewards;
+
+    @FindBy(xpath = "(//input[@placeholder='Enter Certification'])[1]")
+    WebElementFacade selectcertificate;
+
+    @FindBy(xpath = "//input[@id='thoughtLeadership']")
+    WebElementFacade leadership;
+
+    @FindBy(xpath = "//textarea[@id='summary']")
+    WebElementFacade Summary;
+
+    @FindBy(xpath = "//input[@id='projectName_1']")
+    WebElementFacade project1;
+
+    @FindBy(xpath = "(//select[@name='projectType'])[2]")
+    WebElementFacade projecttype;
+
+    @FindBy(xpath = "(//input[@placeholder='Project Domain'])[2]")
+    WebElementFacade domain;
+
+    @FindBy(xpath = "//input[@id='teamSize_1']")
+    WebElementFacade teamsize1;
+
+    @FindBy(xpath = "//input[@id='database_1']")
+    WebElementFacade database1;
+
+    @FindBy(xpath = "//input[@id='framework_1']")
+    WebElementFacade framework1;
+
+    @FindBy(xpath = "//input[@id='PMTool_1']")
+    WebElementFacade PMTOOL;
+
+    @FindBy(xpath = "(//input[@placeholder='Modules'])[2]")
+    WebElementFacade modules;
+
+    @FindBy(xpath = "(//input[@placeholder='Extensions'])[2]")
+    WebElementFacade extension;
+
+    @FindBy(xpath = "(//input[@placeholder='Technology'])[2]")
+    WebElementFacade tech1;
+
+    @FindBy(xpath = "(//input[@placeholder='Third-Party Libraries'])[2]")
+    WebElementFacade projectthirdparty;
+
+    @FindBy(xpath = "//textarea[@id='description_1']")
+    WebElementFacade descriptions;
+
+    @FindBy(xpath = "//textarea[@id='roles_1']")
+    WebElementFacade roles1;
+
+    @FindBy(id = "submit")
+    WebElementFacade Submit;
+
+    @FindBy(id = "durationInMonths_1")
+    WebElementFacade duration;
+
+    @FindBy(xpath = "//*[@id='dataTable']/tbody/tr[1]/td[1]")
+    WebElementFacade firstResume;
+
+    @FindBy(xpath = "(//button[text()='Remove'])[2]")
+    WebElementFacade remove;
+
+    @FindBy(xpath = "//span[text()='Please add at least one project.']")
+    WebElementFacade warningerror;
+
+    @FindBy(xpath = "//span[text()='SHIWANI SHARMA']")
+    WebElementFacade profile;
+
+    @FindBy(xpath = "//a[@href='/account/logout']")
+    WebElementFacade logout;
+
+    @FindBy(xpath = "// button[@id='projectData']")
+    WebElementFacade addProject;
+
+    @FindBy(xpath = "(//a[@title='View Resume'])[1]")
+    WebElementFacade view;
+
+    @FindBy(xpath = "(//a[@title='Edit Resume'])[1]")
+    WebElementFacade edit;
+
+    @FindBy(xpath = "(//a[@title='Download Resume'])[1]")
+    WebElementFacade download;
+
+    @FindBy(id = "saveAndView")
+    WebElementFacade saveandview;
+
+    @FindBy(xpath = "//a[text()='Download This Resume']")
+    WebElementFacade downloadresume;
+
+    @FindBy(xpath = "//a[text()='Edit This Resume']")
+    WebElementFacade editresume;
+
+//    JavascriptExecutor js = (JavascriptExecutor) getDriver();
 
 
     public void navigatetourl() {
         openUrl(config.getString("loginTestUrl"));
         getDriver().manage().window().maximize();
+
     }
+
 
     public void loginwithgoogle() {
-        waitFor(login).click();
+        //  js.executeScript("arguments[0].click();", login);
+
+        waitFor(login).waitUntilVisible().withTimeoutOf(120, TimeUnit.SECONDS).click();
     }
 
-    public void email(String email)
-    {
-        this.e_mail=email;
-//        waitFor(EMAIL).sendKeys(e_mail);
-        waitFor(EMAIL).waitUntilVisible().withTimeoutOf(20, TimeUnit.SECONDS).sendKeys(email);
+    public void email(String emails) {
 
-    }
-
-    public void next()
-    {
-        waitFor(next).click();
-        getDriver().quit();
+        waitFor(EMAIL).waitUntilVisible().withTimeoutOf(120, TimeUnit.SECONDS).sendKeys(emails);
 
     }
 
-    public void Password()
-    {
+    public void next() {
+        // js.executeScript("arguments[0].click();", Next);
+        waitFor(Next).waitUntilClickable().withTimeoutOf(60, TimeUnit.SECONDS).click();
+        waitABit(5000);
 
-        waitFor(Password).sendKeys("Lovelyjhansi@123");
-        waitFor(next).click();
-        waitABit(15000);
 
     }
 
-    public void windowSwitch()
-    {
+    public void Password() {
+        waitFor(Password).waitUntilVisible().withTimeoutOf(60, TimeUnit.SECONDS).sendKeys("Lovelyjhansi@123");
+    }
 
-        ArrayList<String> availableWindows = new ArrayList<String>(getDriver().getWindowHandles());
-        if (!availableWindows.isEmpty()) {
-            getDriver().switchTo().window(availableWindows.get(1));
+    public void windowSwitch() {
+
+//        ArrayList<String> availableWindows = new ArrayList<String>(getDriver().getWindowHandles());
+//        if (!availableWindows.isEmpty()) {
+//            getDriver().switchTo().window(availableWindows.get(1));
+//            getDriver().switchTo().parentFrame();
+
+
+
+            Set<String> windows = getDriver().getWindowHandles();
+            for (String ws : windows) {
+                getDriver().switchTo().window(ws);
+
+
+            }
+
 
         }
+
+
+    public void createResume() {
+//        String Parent=getDriver().getWindowHandle();
+//        getDriver().switchTo().window(Parent);
+        windowSwitch();
+        waitABit(10000);
+        waitFor(createResumes).waitUntilClickable().withTimeoutOf(120, TimeUnit.SECONDS).click();
+
+    }
+
+
+    public void title() {
+        waitFor(Title).waitUntilVisible().withTimeoutOf(60, TimeUnit.SECONDS).sendKeys("QUALITY ANALYST");
+    }
+
+    public void headline() {
+        waitFor(Headline).waitUntilVisible().withTimeoutOf(60, TimeUnit.SECONDS).sendKeys("Headlineee");
+    }
+
+
+    public void phone() {
+        waitFor(Phone).waitUntilVisible().withTimeoutOf(60, TimeUnit.SECONDS).sendKeys("0987654321");
+    }
+
+
+    public void totalExperience() {
+        waitFor(totalExperienceInMonths).waitUntilVisible().withTimeoutOf(60, TimeUnit.SECONDS).sendKeys("5");
+    }
+
+
+    public void linkedIn() {
+
+        waitFor(LinkedIn).waitUntilVisible().withTimeoutOf(60, TimeUnit.SECONDS).sendKeys("https://www.linkedin.com/in/vaishali-jain-8a7854171");
+
+
+    }
+
+    public void github() {
+        waitFor(githublink).waitUntilVisible().withTimeoutOf(60, TimeUnit.SECONDS).sendKeys("https://www.linkedin.com/in/vaishali-jain-8a7854171");
+    }
+
+
+    public void bitbucket() {
+        waitFor(Bitbucket).waitUntilVisible().withTimeoutOf(60, TimeUnit.SECONDS).sendKeys("https://www.linkedin.com/in/vaishali-jain-8a7854171");
+    }
+
+    public void stackOverflow() {
+        waitFor(stacklink).waitUntilVisible().withTimeoutOf(60, TimeUnit.SECONDS).sendKeys("https://www.linkedin.com/in/vaishali-jain-8a7854171");
+    }
+
+    public void hackerRank() {
+        waitFor(HackerRank).waitUntilVisible().withTimeoutOf(60, TimeUnit.SECONDS).sendKeys("https://www.linkedin.com/in/vaishali-jain-8a7854171");
+
+        ((JavascriptExecutor) getDriver()).executeScript(("scroll(0, 500);"));
+    }
+
+    public void technologies() {
+        waitFor(Technologies).waitUntilVisible().withTimeoutOf(60, TimeUnit.SECONDS).sendKeys("c,java,python");
+        Technologies.sendKeys(Keys.ENTER);
+
+    }
+
+    public void database() {
+        waitFor(Database).waitUntilVisible().withTimeoutOf(60, TimeUnit.SECONDS).sendKeys("DATABASE");
+        Database.sendKeys(Keys.ENTER);
+    }
+
+    public void frameworks() {
+        waitFor(Framework).waitUntilVisible().withTimeoutOf(60, TimeUnit.SECONDS).sendKeys("FRAMEWORKS");
+        Framework.sendKeys(Keys.ENTER);
+    }
+
+    public void os() {
+
+        waitFor(OS).waitUntilVisible().withTimeoutOf(60, TimeUnit.SECONDS).sendKeys("WINDOWS", "LINUX", "ANDROID");
+        OS.sendKeys(Keys.ENTER);
+
+    }
+
+    public void thirdPartylibraries() {
+        waitFor(thirdparty).waitUntilVisible().withTimeoutOf(60, TimeUnit.SECONDS).sendKeys("third party");
+        thirdparty.sendKeys(Keys.ENTER);
+    }
+
+    public void rewardsAndrecognition() {
+        waitFor(rewards).waitUntilVisible().withTimeoutOf(60, TimeUnit.SECONDS).sendKeys("REWARDS");
+
+    }
+
+    public void certifications() {
+
+
+        waitFor(selectcertificate).waitUntilVisible().withTimeoutOf(60, TimeUnit.SECONDS).sendKeys("certifications");
+    }
+
+
+    public void thoughtLeadership() {
+        waitFor(leadership).waitUntilVisible().withTimeoutOf(60, TimeUnit.SECONDS).sendKeys("Team leader");
+    }
+
+
+    public void summary() {
+        waitFor(Summary).waitUntilVisible().withTimeoutOf(60, TimeUnit.SECONDS).sendKeys("I have been in successive for last 10 months");
+    }
+
+
+
+    public void projectName() {
+        waitFor(project1).waitUntilVisible().withTimeoutOf(60, TimeUnit.SECONDS).sendKeys("Office and dragons");
+    }
+
+
+    public void projectType() {
+
+        WebElement dropdown = getDriver().findElement(By.xpath("(//select[@name='projectType'])[2]"));
+        Select select = new Select(dropdown);
+        select.selectByIndex(3);
+    }
+
+    public void projectDomain() {
+
+        waitFor(domain).waitUntilVisible().withTimeoutOf(60, TimeUnit.SECONDS).sendKeys("QUALITY ASSURANCE");
+    }
+
+    public void teamSize() {
+        waitFor(teamsize1).waitUntilVisible().withTimeoutOf(60, TimeUnit.SECONDS).sendKeys("5");
+    }
+
+    public void projectDatabases() {
+
+        waitFor(database1).waitUntilVisible().withTimeoutOf(60, TimeUnit.SECONDS).sendKeys("DATABASES2");
+    }
+
+
+    public void projectFramework() {
+        waitFor(framework1).waitUntilVisible().withTimeoutOf(60, TimeUnit.SECONDS).sendKeys("frameworks");
+        framework1.sendKeys(Keys.ENTER);
+
+    }
+
+
+    public void managementTools() {
+        waitFor(PMTOOL).waitUntilVisible().withTimeoutOf(60, TimeUnit.SECONDS).sendKeys("Management tools");
+    }
+
+
+    public void duration() {
+        waitFor(duration).waitUntilVisible().withTimeoutOf(60, TimeUnit.SECONDS).sendKeys("10");
+    }
+
+    public void modulesDeveloped() {
+        waitFor(modules).waitUntilVisible().withTimeoutOf(60, TimeUnit.SECONDS).sendKeys("modules");
+    }
+
+
+    public void extensionsUsed() {
+        waitFor(extension).waitUntilVisible().withTimeoutOf(60, TimeUnit.SECONDS).sendKeys("Extension used");
+    }
+
+
+    public void projectTechnology() {
+        waitFor(tech1).waitUntilVisible().withTimeoutOf(60, TimeUnit.SECONDS).sendKeys("c,java,python");
+        tech1.sendKeys(Keys.ENTER);
+
+    }
+
+
+    public void projectThirtparty() {
+        waitFor(projectthirdparty).waitUntilVisible().withTimeoutOf(60, TimeUnit.SECONDS).sendKeys("projectthirdparty");
+    }
+
+
+    public void description() {
+        waitFor(descriptions).waitUntilVisible().withTimeoutOf(60, TimeUnit.SECONDS).sendKeys("No description for now");
+    }
+
+
+    public void roles() {
+        waitFor(roles1).waitUntilVisible().withTimeoutOf(60, TimeUnit.SECONDS).sendKeys("Team lead");
+    }
+
+    public void submit() {
+        waitFor(Submit).waitUntilVisible().withTimeoutOf(10, TimeUnit.SECONDS).click();
+
+    }
+
+    public void verifyResume() {
+        String firstRow = waitFor(firstResume).getText();
+        Assert.assertEquals(firstRow, "QUALITY ANALYST");
+        System.out.println("Resume verified");
+    }
+
+    public void remove() {
+        waitFor(remove).waitUntilVisible().withTimeoutOf(10, TimeUnit.SECONDS).click();
+        ((JavascriptExecutor) getDriver()).executeScript(("scroll(0, 500);"));
+
+
+    }
+
+    public void warning() {
+
+        String message = warningerror.getText().trim();
+        Assert.assertEquals(message, "Please add at least one project.".trim());
+        System.out.println("Verified hint");
+
+    }
+
+    public void profileicon() {
+        waitFor(profile).waitUntilVisible().withTimeoutOf(10, TimeUnit.SECONDS).click();
+        ;
+    }
+
+    public void logout() {
+        waitFor(logout).waitUntilVisible().withTimeoutOf(10, TimeUnit.SECONDS).click();
+
+
+    }
+
+
+    public void addProject() {
+        waitFor(addProject).waitUntilVisible().withTimeoutOf(10, TimeUnit.SECONDS).click();
+
+    }
+
+    public void verifyProjectsection() {
+        waitFor(project1).waitUntilPresent().isDisplayed();
+        waitFor(projecttype).waitUntilPresent().isDisplayed();
+        waitFor(domain).waitUntilPresent().isDisplayed();
+
+    }
+
+
+    public void Invalidphone(String phone_no) {
+        waitFor(Phone).waitUntilVisible().withTimeoutOf(60, TimeUnit.SECONDS).sendKeys(phone_no);
+
+    }
+
+    public void verifycss() {
+        String color = waitFor(Phone).getCssValue("color");
+        Assert.assertEquals("rgba(110, 112, 126, 1)", color);
+        System.out.println("Error color have been verified");
+    }
+
+    public void view() {
+        waitFor(view).waitUntilVisible().withTimeoutOf(10, TimeUnit.SECONDS).isVisible();
+
+    }
+
+
+    public void download() {
+        waitFor(download).waitUntilVisible().withTimeoutOf(10, TimeUnit.SECONDS).isVisible();
+
+    }
+
+    public void edit() {
+        waitFor(edit).waitUntilVisible().withTimeoutOf(10, TimeUnit.SECONDS).isVisible();
+
+    }
+
+    public void editclick() {
+        waitFor(edit).waitUntilVisible().withTimeoutOf(10, TimeUnit.SECONDS).click();
+
+    }
+
+    public void submitAndview() {
+        waitFor(saveandview).waitUntilVisible().withTimeoutOf(10, TimeUnit.SECONDS).isVisible();
+
+    }
+
+    public void submitdisplay() {
+        waitFor(Submit).waitUntilVisible().withTimeoutOf(10, TimeUnit.SECONDS).isVisible();
+
+    }
+
+    public void submitAndviewclick() {
+        waitFor(saveandview).waitUntilVisible().withTimeoutOf(10, TimeUnit.SECONDS).click();
+
+    }
+
+    public void downloadOneditPage() {
+        waitFor(downloadresume).waitUntilVisible().withTimeoutOf(10, TimeUnit.SECONDS).isVisible();
+
+    }
+
+    public void editOneditPage() {
+        waitFor(editresume).waitUntilVisible().withTimeoutOf(10, TimeUnit.SECONDS).isVisible();
+
+    }
+
+    public void removebtn() {
+        waitFor(remove).waitUntilVisible().withTimeoutOf(10, TimeUnit.SECONDS).isVisible();
+
+    }
+
+    public void addnewbtn() {
+        waitFor(addProject).waitUntilVisible().withTimeoutOf(10, TimeUnit.SECONDS).isVisible();
+
+
     }
 }
--- src/main/java/net/test/Steps/Resume_steps.java
+++ src/main/java/net/test/Steps/Resume_steps.java
@@ -1,46 +1,248 @@
 package net.test.Steps;
-
-import cucumber.api.PendingException;
-import cucumber.api.java.en.And;
-import cucumber.api.java.en.When;
 import net.test.Pages.Resumepage;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 import cucumber.api.java.en.Given;
 
-public class Resume_steps  extends Resumepage
-{
+public class Resume_steps extends Resumepage {
 
-   Resumepage pagess;
+    Resumepage pagess;
+
 
     @Given("^user is on resume builder loginpage$")
-    public void userIsOnResumeBuilderLoginpage()
-    {
+    public void userIsOnResumeBuilderLoginpage() {
         pagess.navigatetourl();
+
     }
 
-    @When("^user choose login with gmail$")
-    public void userChooseLoginWithGmail()
-    {
-        pagess.loginwithgoogle();
 
+    @When("^user choose login with gmail$")
+    public void userChooseLoginWithGmail() {
+        pagess.loginwithgoogle();
     }
 
 
     @And("^user will enter password$")
-    public void userWillEnterPassword()
-    {
+    public void userWillEnterPassword() {
         pagess.Password();
+        pagess.next();
 
     }
 
+
     @And("^enter successive domain \"([^\"]*)\"id$")
-    public void enterSuccessiveDomainId(String email)
-    {
+    public void enterSuccessiveDomainId(String emails) {
         pagess.windowSwitch();
-        pagess.email(email);
+        pagess.email(emails);
         pagess.next();
         waitABit(5000);
 
+    }
 
+    @Given("^user will click on create resume button$")
+    public void userWillClickOnCreateResumeButton() {
+        pagess.createResume();
+
+    }
+
+    @When("^user enetr all the field's data in form$")
+    public void userEnetrAllTheFieldSDataInForm() {
+        pagess.title();
+        pagess.headline();
+        pagess.phone();
+        pagess.totalExperience();
+        pagess.linkedIn();
+        pagess.github();
+        pagess.bitbucket();
+        pagess.stackOverflow();
+        pagess.hackerRank();
+        pagess.technologies();
+        pagess.database();
+        pagess.frameworks();
+        pagess.os();
+        pagess.thirdPartylibraries();
+        pagess.rewardsAndrecognition();
+        pagess.certifications();
+        pagess.thoughtLeadership();
+        pagess.summary();
+    }
+
+    @And("^user will enter first project sections details$")
+    public void userWillEnterFirstProjectSectionsDetails() {
+        pagess.projectName();
+        pagess.projectType();
+        pagess.projectDomain();
+        pagess.teamSize();
+        pagess.projectDatabases();
+        pagess.projectFramework();
+        pagess.managementTools();
+        pagess.duration();
+        pagess.modulesDeveloped();
+        pagess.extensionsUsed();
+        pagess.projectTechnology();
+        pagess.projectThirtparty();
+        pagess.description();
+        pagess.roles();
+    }
+
+    @And("^click on submit button$")
+    public void clickOnSubmitButton() {
+        pagess.submit();
+    }
+
+    @Then("^user will land on dashboard and create resume will be shown in first row$")
+    public void userWillLandOnDashboardAndCreateResumeWillBeShownInFirstRow() {
+        pagess.verifyResume();
+
+    }
+
+    @Given("^User is on create resume page$")
+    public void userIsOnCreateResumePage() {
+        pagess.createResume();
+    }
+
+    @And("^user will enter first half details$")
+    public void userWillEnterFirstHalfDetails() {
+        pagess.title();
+        pagess.headline();
+        pagess.phone();
+        pagess.totalExperience();
+        pagess.linkedIn();
+        pagess.github();
+        pagess.bitbucket();
+        pagess.stackOverflow();
+        pagess.hackerRank();
+        pagess.technologies();
+        pagess.database();
+        pagess.frameworks();
+        pagess.os();
+        pagess.thirdPartylibraries();
+        pagess.rewardsAndrecognition();
+        pagess.certifications();
+        pagess.thoughtLeadership();
+        pagess.summary();
+    }
+
+    @And("^click on remove button to remove project details part$")
+    public void clickOnRemoveButtonToRemoveProjectDetailsPart() {
+        pagess.remove();
+    }
+
+    @Then("^click on  the submit button$")
+    public void clickOnTheSubmitButton() {
+        pagess.submit();
+
+    }
+
+    @And("^Verify the error message$")
+    public void verifyTheErrorMessage() {
+        pagess.warning();
+    }
+
+
+    @And("^user is already on create resume screen and user click on profile icon$")
+    public void userIsAlreadyOnCreateResumeScreenAndUserClickOnProfileIcon() {
+        pagess.profileicon();
+    }
+
+    @And("^click on logut button$")
+    public void clickOnLogutButton() {
+        pagess.profileicon();
+        pagess.logout();
+
+    }
+
+
+    @Given("^user is on dashboard screen and click logout$")
+    public void userIsOnDashboardScreenAndClickLogout() {
+        pagess.profileicon();
+        pagess.logout();
+    }
+
+    @And("^user click on add project button$")
+    public void userClickOnAddProjectButton() {
+        pagess.addProject();
+    }
+
+    @Then("^verify project section has been added by verifying fields$")
+    public void verifyProjectSectionHasBeenAddedByVerifyingFields() {
+        pagess.verifyProjectsection();
+    }
+
+
+    @And("^user enter \"([^\"]*)\" in the field$")
+    public void userEnterInTheField(String phone_no) {
+        pagess.Invalidphone(phone_no);
+    }
+
+
+    @And("^user click on submit button$")
+    public void userClickOnSubmitButton() {
+        pagess.submit();
+    }
+
+    @Then("^user wont be able to submit the form and verify client side validation$")
+    public void userWontBeAbleToSubmitTheFormAndVerifyClientSideValidation() {
+        pagess.verifycss();
+    }
+
+    @And("^user enter all the field's data in form excluding phone$")
+    public void userEnterAllTheFieldSDataInFormExcludingPhone() {
+        pagess.title();
+        pagess.headline();
+        pagess.totalExperience();
+        pagess.linkedIn();
+        pagess.github();
+        pagess.bitbucket();
+        pagess.stackOverflow();
+        pagess.hackerRank();
+        pagess.technologies();
+        pagess.database();
+        pagess.frameworks();
+        pagess.os();
+        pagess.thirdPartylibraries();
+        pagess.rewardsAndrecognition();
+        pagess.certifications();
+        pagess.thoughtLeadership();
+        pagess.summary();
+    }
+
+    @And("^user verify the edit view and download buttons$")
+    public void userVerifyTheEditViewAndDownloadButtons() {
+        pagess.view();
+        pagess.download();
+        pagess.edit();
+    }
+
+
+    @And("^user click on edit button$")
+    public void userClickOnEditButton() {
+        pagess.editclick();
+    }
+
+    @And("^user will redirect to another page where user will verify Submit and submit and view button$")
+    public void userWillRedirectToAnotherPageWhereUserWillVerifySubmitAndSubmitAndViewButton() {
+        pagess.submitdisplay();
+        pagess.submitAndview();
+    }
+
+    @And("^user will redirect to another page and user click on submit and view button$")
+    public void userWillRedirectToAnotherPageAndUserClickOnSubmitAndViewButton() {
+        pagess.submitAndviewclick();
+    }
+
+    @Then("^user will be redirected to new page where user will verify download the resume and edit resume button$")
+    public void userWillBeRedirectedToNewPageWhereUserWillVerifyDownloadTheResumeAndEditResumeButton() {
+        pagess.downloadOneditPage();
+        pagess.editOneditPage();
+    }
+
+
+    @And("^user will verify remove and add new button$")
+    public void userWillVerifyRemoveAndAddNewButton() {
+        pagess.removebtn();
+        pagess.addnewbtn();
     }
 
 }
